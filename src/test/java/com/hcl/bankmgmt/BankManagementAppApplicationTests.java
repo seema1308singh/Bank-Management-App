8572ed9b95254d953f7361c7a30a4b17b02a7228
swaraj <swarajvarma28@gmail.com>
2022-03-17 17:35:39 +0530

    Initial commit

---
+++ src/test/java/com/hcl/bankmgmt/BankManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.bankmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
